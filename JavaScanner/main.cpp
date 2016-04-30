5b9e29f1f244c25b48e2dd4f41db1161337be292
靳洪博 <aifjhb0815@gmail.com>
2016-04-30 19:41:01 +0800

    Initial Commit

---
+++ JavaScanner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JavaScanner
+//
+//  Created by 靳洪博 on 16/4/30.
+//  Copyright © 2016年 靳洪博. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
